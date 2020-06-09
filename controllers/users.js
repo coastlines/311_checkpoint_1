b3c97d2ddae53f869eef2d25ae854c6d9bbfa87d
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-06-08 22:59:11 -0500

    create user

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,27 @@
 const users = require('../data/index')
 
+// LIST -- GET /users
 const list = (req, res) => {
   res.json(users)
 }
 
+// SHOW -- GET /users/:id
 const show = (req, res) => {
   let user = users.find( user => user.id == req.params.id)
   res.json(user)
 }
 
-module.exports = { list, show }
+// CREATE -- POST /users
+const create = (req, res) => {
+  let newUser = req.body
+  let newId = users.length + 1
+  
+  newUser.id = newId
+  users.push(newUser)
+
+  res.json(users)
+}
+
+// UPDATE -- PUT /users/:id
+
+module.exports = { list, show, create }
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 //const { list, show, create, update, remove } = require('../controllers/users.js')
-const { list, show } = require('../controllers/users.js')
+const { list, show, create, update } = require('../controllers/users.js')
 
 // GET /users
 router.get('/users', list)
@@ -9,11 +9,11 @@ router.get('/users', list)
 // GET /users/:id
 router.get('/users/:id', show)
 
-// // POST /users
-// router.post('/users', create)
+// POST /users
+router.post('/users', create)
 
-// // PUT /users/:id
-// router.put('/users:id', update)
+// PUT /users/:id
+router.put('/users:id', update)
 
 // // DELETE /users/:id
 // router.delete('/users/:id', remove)
