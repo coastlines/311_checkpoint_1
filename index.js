1dd9ca822bb8885e19cb34ca1c8234d6477904a9
efaulkenberry@gmail.com <efaulkenberry@gmail.com>
2020-06-09 00:21:43 -0500

    tidied code

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const express = require('express')
 const bodyParser = require("body-parser")
 const app = express()
 const port = process.env.PORT || 4000
+const userRoute = require('./routes/users')
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('default route'))
-
-const userRoute = require('./routes/users')
-
 app.use(userRoute)
 
+app.get('/', (req, res) => res.send('default route'))
 
 app.listen(port, () => {
   console.log('app is listening on:', port)
